9a6e41797a0baf9948b4ce726102d4039c8d1db3
Victor LaBrie <vplabrie@gmail.com>
2023-07-11 22:02:58 -0500

    Starting work on DracoExporting.

---
+++ exportDraco.js
@@ -0,0 +1,18 @@
+import {DracoExporter} from 'three/addons/exporters/DRACOExporter.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const exporter = new DracoExporter()
+let loader = new GLTFLoader()
+
+let scene = new THREE.scene()
+
+loader.load('/Users/vicclab/Development/Projects/Personal Site/PersonalWebsite/public/decimatedwaterbug/decimatedbug.glb',
+	function(gltf){
+		const binaryData=exporter.parse(gltf.scene)
+		saveArrayBuffer(binaryData, 'mesh.drc')
+	}
+);
+
+
+//const binaryData = exporter.parse("/Users/vicclab/Development/Projects/Personal Site/PersonalWebsite/public/decimatedwaterbug/decimatedbug.glb")
+//saveArrayBuffer(binaryData, 'mesh.drc')
