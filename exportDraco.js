08a9c5990e01d8d0ede650b177085e1c7ad91762
Victor LaBrie <vplabrie@gmail.com>
2023-07-20 23:02:52 -0500

    Getting page set up.

--- exportDraco.js
+++ exportDraco.js
@@ -1,17 +1,18 @@
-import {DracoExporter} from 'three/addons/exporters/DRACOExporter.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+//import {DracoExporter} from 'three/addons/exporters/DRACOExporter.js'
+//import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import './style.css'
 
-const exporter = new DracoExporter()
-let loader = new GLTFLoader()
+// const exporter = new DracoExporter()
+// let loader = new GLTFLoader()
 
-let scene = new THREE.scene()
+// let scene = new THREE.scene()
 
-loader.load('/Users/vicclab/Development/Projects/Personal Site/PersonalWebsite/public/decimatedwaterbug/decimatedbug.glb',
-	function(gltf){
-		const binaryData=exporter.parse(gltf.scene)
-		saveArrayBuffer(binaryData, 'mesh.drc')
-	}
-);
+// loader.load('/Users/vicclab/Development/Projects/Personal Site/PersonalWebsite/public/decimatedwaterbug/decimatedbug.glb',
+// 	function(gltf){
+// 		const binaryData=exporter.parse(gltf.scene)
+// 		saveArrayBuffer(binaryData, 'mesh.drc')
+// 	}
+// );
 
 
 //const binaryData = exporter.parse("/Users/vicclab/Development/Projects/Personal Site/PersonalWebsite/public/decimatedwaterbug/decimatedbug.glb")
