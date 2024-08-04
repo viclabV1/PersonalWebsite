40b0babcfaf9d9cd6ee4c20d3672ae307ccfb589
Victor LaBrie <vplabrie@gmail.com>
2024-08-04 10:56:40 -0500

    fix: resolved resolution issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 const { resolve } = require('path')
 const { defineConfig } = require('vite')
-module.exports = defineConfig({
+
+export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       input: {
         portfolio: resolve(__dirname, 'index.html'),
-        genetic: resolve(__dirname, 'GeneticAlgorithmJS/geneticAlgo3d/index.html'),
+        genetic: resolve(__dirname, './GeneticAlgorithmJS/geneticAlgo3d/index.html'),
         bugpage: resolve(__dirname, 'waterbug.html'),
         robots: resolve(__dirname, './public/robots.txt')
       },
